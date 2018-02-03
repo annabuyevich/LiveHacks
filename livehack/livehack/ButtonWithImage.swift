df776d28ef88420e181f8d546c2717ee519a4d25
annabuyevich <avbuyevich@gmail.com>
2018-02-03 21:21:45 +0100

    added livelack folders

---
+++ livehack/livehack/ButtonWithImage.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonWithImage.swift
+//  livehack
+//
+//  Created by Anna B. on 2/3/18.
+//  Copyright © 2018 Anna Buyevich. All rights reserved.
+//
+
+import UIKit
+
+class ButtonWithImage: UIButton {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if imageView != nil {
+            imageEdgeInsets = UIEdgeInsets(top: 5, left: (bounds.width - 35), bottom: 5, right: 5)
+            titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: (imageView?.frame.width)!)
+        }
+    }
+}
---
+++ livehack/livehack/FloatingHearts.h
@@ -0,0 +1,20 @@
+//
+//  FloatingHearts.h
+//  livehack
+//
+//  Created by Anna B. on 2/3/18.
+//  Copyright © 2018 Anna Buyevich. All rights reserved.
+//
+
+#ifndef FloatingHearts_h
+#define FloatingHearts_h
+
+
+#endif /* FloatingHearts_h */
+
+
+#import <UIKit/UIkit.h>
+
+FOUNDATION_EXPORT double FloatingHeartsVersionNumber;
+
+FOUNDATION_Export const unsigned char FloatingHeartsVersionString[];
